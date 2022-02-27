73af873df4bfce837b4f0b1d8969b44985639694
Lucas Carvalho <lucaseduardo@gmail.com>
2022-02-27 20:41:27 -0300

    Inserção de usuários

--- app/src/main/java/com/example/guestapp/repository/GuestRepository.kt
+++ app/src/main/java/com/example/guestapp/repository/GuestRepository.kt
@@ -1,20 +1,23 @@
 package com.example.guestapp.repository
 
+import android.content.ContentValues
 import android.content.Context
 import com.example.guestapp.database.GuestDataBaseHelper
 import com.example.guestapp.model.GuestModel
+import com.example.guestapp.utils.DataBaseConstants
+import java.lang.Exception
 import java.util.ArrayList
 
-class GuestRepository private constructor(context: Context){
+class GuestRepository private constructor(context: Context) {
 
 
     private var mGuestDataBaseHelper: GuestDataBaseHelper = GuestDataBaseHelper(context)
 
-    companion object{
+    companion object {
         private lateinit var repository: GuestRepository
 
-        fun getInstance(context: Context): GuestRepository{
-            if(!::repository.isInitialized){
+        fun getInstance(context: Context): GuestRepository {
+            if (!::repository.isInitialized) {
                 repository = GuestRepository(context)
             }
             return repository
@@ -22,31 +25,42 @@ class GuestRepository private constructor(context: Context){
     }
 
 
-
-
-    fun getAll(): List<GuestModel>{
+    fun getAll(): List<GuestModel> {
         val list: MutableList<GuestModel> = ArrayList()
         return list
     }
 
-    fun getPresents(): List<GuestModel>{
+    fun getPresents(): List<GuestModel> {
         val list: MutableList<GuestModel> = ArrayList()
         return list
     }
 
-    fun getAbsents(): List<GuestModel>{
+    fun getAbsents(): List<GuestModel> {
         val list: MutableList<GuestModel> = ArrayList()
         return list
     }
 
-    fun delete(guestModel: GuestModel){
-
-    }
-    fun update(guestModel: GuestModel){
+    fun delete(guestModel: GuestModel) {
 
     }
 
-    fun save(guestModel: GuestModel){
+    fun update(guestModel: GuestModel) {
+
+    }
+
+    fun save(guestModel: GuestModel): Boolean {
+        try {
+            var db = mGuestDataBaseHelper.writableDatabase
+
+            val contentValue = ContentValues()
+            contentValue.put(DataBaseConstants.GUEST.COLUMNS.NAME, guestModel.name)
+            contentValue.put(DataBaseConstants.GUEST.COLUMNS.PRESENCE, guestModel.presence)
+            db.insert(DataBaseConstants.GUEST.TABLE_NAME,null,contentValue)
+            return true
+        }catch (e: Exception){
+            return false
+        }
+
 
     }
 }
